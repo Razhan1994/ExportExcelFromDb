2b3ea0f48b0edeac8c7e4e4ce625e0696a776274
JavidKoluRazhan <javidklurajan@gmail.com>
2021-04-25 14:30:25 +0430

    .

--- ExportExcelFromDB/Startup.cs
+++ ExportExcelFromDB/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Service.ExportExcel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,6 +25,8 @@ namespace ExportExcelFromDB
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddSingleton<IExportService, ExportService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
